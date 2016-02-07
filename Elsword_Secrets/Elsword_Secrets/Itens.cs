18c8775a21bb5d6aec55a7fb1d406c98db435377
Adilson Alves <adilson.day@hotmail.com>
2016-02-07 10:27:08 -0200

    Add project files
    Project added start making changes now

---
+++ Elsword_Secrets/Elsword_Secrets/Itens.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elsword_Secrets
+{
+    class Itens
+    {
+        public ItensSecret TipoItem { get; set; }   
+        
+
+        public static void AddItem()
+        {
+
+        }
+    }
+
+    enum ItensSecret
+    {
+        ElderMoeda,
+        BesmaMoeda,
+        AlteraMoeda,
+        VelderMoeda,
+        HamelMoeda
+    }
+}
